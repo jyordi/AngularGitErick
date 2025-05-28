7d6f7cf043f2859350804557c6a74ac81c03d550
Mildred Alely <mildredalely07@gmail.com>
2025-05-28 09:40:50 -0600

    modificar el ts

--- src/app/usuarios/usuario/usuario.component.ts
+++ src/app/usuarios/usuario/usuario.component.ts
@@ -8,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './usuario.component.css'
 })
 export class UsuarioComponent {
+   usuarios = [
+    { nombre: 'Juan Pérez', correo: 'juanperez@email.com' },
+    { nombre: 'María Gómez', correo: 'maria.gomez@email.com' },
+    { nombre: 'Carlos Díaz', correo: 'carlos.diaz@email.com' }
+  ];
+
+  verUsuario(usuario: any) {
+    console.log('Ver:', usuario);
+  }
+
+  editarUsuario(usuario: any) {
+    console.log('Editar:', usuario);
+  }
+
+  eliminarUsuario(usuario: any) {
+    console.log('Eliminar:', usuario);
+  }
+
+  agregarUsuario() {
+    console.log('Agregar nuevo usuario');
+  }
 
 }
